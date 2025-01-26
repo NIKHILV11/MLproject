672a0ea0c81c1f249e76a8ad9dbffdb4149c12be
NIKHILV123 <v.nikhil.0005@gmail.com>
2025-01-26 22:08:14 +0530

    Notebook

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass ## For creating a class and its attributes in a single line of code, to store the data
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts',"train.csv") ## The data will be stored in the artifacts folder in the form of a train.csv file
@@ -41,8 +43,13 @@ class DataIngestion:
             raise CustomException(e,sys)
 
 if __name__ == "__main__":
+
     obj = DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
+
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_array,test_array,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_array,test_array))
 
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ from src.logger import logging
 import dill
 import numpy as np
 import pandas as pd
+from sklearn.metrics import  r2_score
 
 def save_object(file_path,obj):
     try:
@@ -12,5 +13,18 @@ def save_object(file_path,obj):
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path,"wb") as file_obj:
             dill.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def evaluate_models(X_train,y_train,X_test,y_test,models,preprocessing_obj):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            model.fit(X_train,y_train)
+            y_test_pred=model.predict(X_test)
+            test_model_score=r2_score(y_test,y_test_pred)
+            report[list(models.keys())[i]]=test_model_score
+        return report
     except Exception as e:
         raise CustomException(e,sys)
